54eb8c73fbe47f4a8af2485a2fcdb8eb7ccead6a
Masha <bogdanovamasha.2009.2021@gmail.com>
2024-12-05 22:04:41 +0300

    есть

--- src/linear.clj
+++ src/linear.clj
@@ -11,7 +11,10 @@
 
 (def vd (partial v-operation /))
 
-(defn dot [& args] (apply + (apply v* args)))
+(defn dot [& args]
+  (if (empty? args)
+    0
+    (apply + (apply v* args))))
 
 (defn v*s [v & args]
   (mapv #(* (apply * args) %) v))
@@ -30,11 +33,18 @@
 (defn m*s [v & args]
   (mapv #(apply v*s % args) v))
 
-(defn transpose [& args])
+(defn transpose [args]
+  (apply mapv vector args))
 
 (defn m*v [m v]
   (mapv #(dot v %) m))
 
-(defn m*m [m1 m2])
+(defn m*m-two [m1 m2]
+  (mapv #(m*v (transpose m2) %) m1))
 
-;(println )
+(defn m*m [& args]
+  (reduce m*m-two args))
+
+;(println (m*m [[1 2] [3 4] [5 6] [7 8]]
+;              [[1000 100 10] [1 0.1 0.01]]
+;              [[11] [22] [33]]))
